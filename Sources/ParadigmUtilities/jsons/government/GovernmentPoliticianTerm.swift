0ab0a1abc1b63079814c7285fb4300627da35d8f
RandomHashTags <ImRandomHashTags@gmail.com>
2022-10-11 18:29:08 -0500

    corrected the visibility of 1 struct initializer

--- Sources/ParadigmUtilities/jsons/government/GovernmentPoliticianTerm.swift
+++ Sources/ParadigmUtilities/jsons/government/GovernmentPoliticianTerm.swift
@@ -11,7 +11,7 @@ import SwiftSovereignStates
 public struct GovernmentPoliticianTerm : Jsonable {
     public let position:GovernmentPoliticianPosition, start:EventDate, end:EventDate?, party:PoliticalParty, how:String?, subdivision:SovereignStateSubdivisionWrapper?, district:Int?, contactInformation:ContactInformation?
     
-    init(position: GovernmentPoliticianPosition, start: EventDate, end: EventDate?, party: PoliticalParty, how: String?, subdivision: (any SovereignStateSubdivision)?, district: Int?, contactInformation: ContactInformation?) {
+    public init(position: GovernmentPoliticianPosition, start: EventDate, end: EventDate?, party: PoliticalParty, how: String?, subdivision: (any SovereignStateSubdivision)?, district: Int?, contactInformation: ContactInformation?) {
         self.position = position
         self.start = start
         self.end = end
