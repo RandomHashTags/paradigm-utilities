069eab797817d20361f2f7eb294320a4fe25006a
RandomHashTags <ImRandomHashTags@gmail.com>
2022-11-02 17:36:27 -0500

    1 fix relating to `SovereignStateRankingInfoValue`

--- Sources/ParadigmUtilities/jsons/countries/information/values/SovereignStateRankingInfoValue.swift
+++ Sources/ParadigmUtilities/jsons/countries/information/values/SovereignStateRankingInfoValue.swift
@@ -17,13 +17,13 @@ public struct SovereignStateRankingInfoValue : SovereignStateRankedValue {
     public let isEstimate:Bool
     public var values:[SovereignStateRankingInfoValueOther]?, sources:EventSources?
     
-    public init(info: SovereignStateInfo, defcon: Int, worldRank: Int, maxWorldRank: Int, yearOfData: Int, value: Int, suffix: String, isEstimate: Bool, values: [SovereignStateRankingInfoValueOther]?, sources: EventSources?) {
+    public init(info: SovereignStateInfo, defcon: Int, worldRank: Int, maxWorldRank: Int, yearOfData: Int, value: Int, suffix: String?, isEstimate: Bool, values: [SovereignStateRankingInfoValueOther]?, sources: EventSources?) {
         self.init(info: info, defcon: defcon, worldRank: worldRank, maxWorldRank: maxWorldRank, yearOfData: yearOfData, value: Double(value), valueType: .integer, suffix: suffix, isEstimate: isEstimate, values: values, sources: sources)
     }
-    public init(info: SovereignStateInfo, defcon: Int, worldRank: Int, maxWorldRank: Int, yearOfData: Int, value: Int64, suffix: String, isEstimate: Bool, values: [SovereignStateRankingInfoValueOther]?, sources: EventSources?) {
+    public init(info: SovereignStateInfo, defcon: Int, worldRank: Int, maxWorldRank: Int, yearOfData: Int, value: Int64, suffix: String?, isEstimate: Bool, values: [SovereignStateRankingInfoValueOther]?, sources: EventSources?) {
         self.init(info: info, defcon: defcon, worldRank: worldRank, maxWorldRank: maxWorldRank, yearOfData: yearOfData, value: Double(value), valueType: .long, suffix: suffix, isEstimate: isEstimate, values: values, sources: sources)
     }
-    public init(info: SovereignStateInfo, defcon: Int, worldRank: Int, maxWorldRank: Int, yearOfData: Int, value: Double, valueType: NumberType, suffix: String, isEstimate: Bool, values: [SovereignStateRankingInfoValueOther]?, sources: EventSources?) {
+    public init(info: SovereignStateInfo, defcon: Int, worldRank: Int, maxWorldRank: Int, yearOfData: Int, value: Double, valueType: NumberType, suffix: String?, isEstimate: Bool, values: [SovereignStateRankingInfoValueOther]?, sources: EventSources?) {
         self.info = info
         self.defcon = defcon
         self.worldRank = worldRank
