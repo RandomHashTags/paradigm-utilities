fdeec259ecbe29984808288c3355c8e6c37bdb20
RandomHashTags <ImRandomHashTags@gmail.com>
2023-01-21 14:57:09 -0600

    update README, and made `weather` omittable from the `HomeResponse`

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # paradigm-utilities
 
-A description of this package.
+This is used as a compatability layer to provide the Server and Client with identical data interpretation and manipulation.
+
+Some features are used only server-side, while others only client-side, but some require both to work properly with each other.
--- Sources/ParadigmUtilities/responses/home/HomeResponse.swift
+++ Sources/ParadigmUtilities/responses/home/HomeResponse.swift
@@ -16,14 +16,14 @@ public struct HomeResponse : Jsonable {
     @CodableOmittable public var government:HomeResponseGovernment?
     @CodableOmittable public var stock_market:HomeResponseStockMarket?
     public var upcoming_events:HomeResponseUpcomingEvents?
-    public var weather:HomeResponseWeather?
+    @CodableOmittable public var weather:HomeResponseWeather?
     
     public init(countries: HomeResponseCountries?, government: HomeResponseGovernment?, stock_market: HomeResponseStockMarket?, upcoming_events: HomeResponseUpcomingEvents?, weather: HomeResponseWeather?) {
         self._countries = CodableOmittable(countries)
         self._government = CodableOmittable(government)
         self._stock_market = CodableOmittable(stock_market)
         self.upcoming_events = upcoming_events
-        self.weather = weather
+        self._weather = CodableOmittable(weather)
     }
     
     public func getKeyValue(key: HomeResponseValueKeys) -> Any? {
@@ -32,7 +32,7 @@ public struct HomeResponse : Jsonable {
         case .government: return _government
         case .stock_market: return _stock_market
         case .upcoming_events: return upcoming_events
-        case .weather: return weather
+        case .weather: return _weather
         }
     }
     public mutating func setKeyValue<T>(key: HomeResponseValueKeys, value: T) {
@@ -50,7 +50,7 @@ public struct HomeResponse : Jsonable {
             upcoming_events = value as? HomeResponseUpcomingEvents
             break
         case .weather:
-            weather = value as? HomeResponseWeather
+            _weather = value as! CodableOmittable<HomeResponseWeather>
             break
         }
     }
@@ -68,7 +68,7 @@ public enum HomeResponseValueKeys : String, JsonableValueKeys {
     }
     public func isOmittable() -> Bool {
         switch self {
-        case .countries, .government, .stock_market:
+        case .countries, .government, .stock_market, .weather:
             return true
         default:
             return false
