00ea85e65924a0faaad68471a91e87324108ee68
RandomHashTags <ImRandomHashTags@gmail.com>
2023-01-21 14:46:11 -0600

    added missing value keys for `HomeResponseUpcomingEventHolidaysResponse`

--- Sources/ParadigmUtilities/responses/upcoming events/HomeResponseUpcomingEventHolidaysResponse.swift
+++ Sources/ParadigmUtilities/responses/upcoming events/HomeResponseUpcomingEventHolidaysResponse.swift
@@ -8,11 +8,34 @@
 import Foundation
 
 public struct HomeResponseUpcomingEventHolidaysResponse : Jsonable {
+    public typealias ValueKeys = HomeResponseUpcomingEventHolidaysResponseValueKeys
+    
     public let date:EventDate
-    public let holidays:[PreHoliday]
+    public var holidays:[PreHoliday]
     
     public init(date: EventDate, holidays: [PreHoliday]) {
         self.date = date
         self.holidays = holidays
+    }
+    
+    public func getKeyValue(key: HomeResponseUpcomingEventHolidaysResponseValueKeys) -> Any? {
+        switch key {
+        case .holidays: return holidays
+        }
+    }
+    public mutating func setKeyValue<T>(key: HomeResponseUpcomingEventHolidaysResponseValueKeys, value: T) {
+        switch key {
+        case .holidays:
+            holidays = value as! [PreHoliday]
+            break
+        }
+    }
+}
+
+public enum HomeResponseUpcomingEventHolidaysResponseValueKeys : String, JsonableValueKeys {
+    case holidays
+    
+    public func isTranslatable() -> Bool {
+        return true
     }
 }
