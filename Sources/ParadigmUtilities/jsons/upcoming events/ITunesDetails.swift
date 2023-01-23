c645b87c0bb50b22104683e3078f8466e50954d4
RandomHashTags <ImRandomHashTags@gmail.com>
2023-01-23 15:57:45 -0600

    updated variable name syntax for `ITunesDetails`

--- Sources/ParadigmUtilities/jsons/upcoming events/ITunesDetails.swift
+++ Sources/ParadigmUtilities/jsons/upcoming events/ITunesDetails.swift
@@ -8,12 +8,12 @@
 import Foundation
 
 public struct ITunesDetails : Jsonable {
-    public let artistViewUrl:String, collectionViewUrl:String, amgArtistId:Int?, imageURL:String
+    public let artist_view_url:String, collection_view_url:String, amg_artist_id:Int?, image_url:String
     
-    public init(artistViewUrl: String, collectionViewUrl: String, amgArtistId: Int?, imageURL: String) {
-        self.artistViewUrl = artistViewUrl
-        self.collectionViewUrl = collectionViewUrl
-        self.amgArtistId = amgArtistId
-        self.imageURL = imageURL
+    public init(artist_view_url: String, collection_view_url: String, amg_artist_id: Int?, image_url: String) {
+        self.artist_view_url = artist_view_url
+        self.collection_view_url = collection_view_url
+        self.amg_artist_id = amg_artist_id
+        self.image_url = image_url
     }
 }
