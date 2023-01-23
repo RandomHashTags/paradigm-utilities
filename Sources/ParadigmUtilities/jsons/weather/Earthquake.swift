3cffcafdd684e4d4bfbc2a35cd9c0fa182044757
RandomHashTags <ImRandomHashTags@gmail.com>
2023-01-23 16:48:09 -0600

    updated variable name syntax for `Earthquake`

--- Sources/ParadigmUtilities/jsons/weather/Earthquake.swift
+++ Sources/ParadigmUtilities/jsons/weather/Earthquake.swift
@@ -13,19 +13,19 @@ public struct Earthquake : Jsonable {
     
     public let country:Country?, subdivision:SovereignStateSubdivisionWrapper?, city:SovereignStateCityWrapper?
     public var cause:String
-    public let magnitude:Float, place:String, exactTimeMilliseconds:Int64, lastUpdated:Int64, depthKM:Float?, location:Location
+    public let magnitude:Float, place:String, exact_time:Int64, last_updated:Int64, depth_km:Float?, location:Location
     public var sources:EventSources
     
-    public init(country: Country?, subdivision: (any SovereignStateSubdivision)?, city: (any SovereignStateCity)?, cause: String, magnitude: Float, place: String, exactTimeMilliseconds: Int64, lastUpdated: Int64, depthKM: Float?, location: Location, sources: EventSources) {
+    public init(country: Country?, subdivision: (any SovereignStateSubdivision)?, city: (any SovereignStateCity)?, cause: String, magnitude: Float, place: String, exact_time: Int64, last_updated: Int64, depth_km: Float?, location: Location, sources: EventSources) {
         self.country = country
         self.subdivision = subdivision?.wrapped()
         self.city = city?.wrapped()
         self.cause = cause
         self.magnitude = magnitude
         self.place = place
-        self.exactTimeMilliseconds = exactTimeMilliseconds
-        self.lastUpdated = lastUpdated
-        self.depthKM = depthKM
+        self.exact_time = exact_time
+        self.last_updated = last_updated
+        self.depth_km = depth_km
         self.location = location
         self.sources = sources
     }
@@ -38,9 +38,9 @@ public struct Earthquake : Jsonable {
         case .cause: return cause
         case .magnitude: return magnitude
         case .place: return place
-        case .exactTimeMilliseconds: return exactTimeMilliseconds
-        case .lastUpdated: return lastUpdated
-        case .depthKM: return depthKM
+        case .exact_time: return exact_time
+        case .last_updated: return last_updated
+        case .depth_km: return depth_km
         case .location: return location
         case .sources: return sources
         }
@@ -66,9 +66,9 @@ public enum EarthquakeValueKeys : String, JsonableValueKeys {
     case cause
     case magnitude
     case place
-    case exactTimeMilliseconds
-    case lastUpdated
-    case depthKM
+    case exact_time
+    case last_updated
+    case depth_km
     case location
     case sources
     
