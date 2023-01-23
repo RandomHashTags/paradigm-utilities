a881c8286e18d878d644282160ebd9c4cfa12115
RandomHashTags <ImRandomHashTags@gmail.com>
2023-01-23 16:11:16 -0600

    updated variable name syntax for `MLBEvent`

--- Sources/ParadigmUtilities/jsons/upcoming events/events/MLBEvent.swift
+++ Sources/ParadigmUtilities/jsons/upcoming events/events/MLBEvent.swift
@@ -8,39 +8,39 @@
 import Foundation
 import SwiftSovereignStates
 
-public enum MLBEventCodingKeys : String, UpcomingEventValueKeys {
-    case teamAway
-    case teamHome
-    
-    public func getCategory() -> UpcomingEventValueCategory {
-        return UpcomingEventValueCategory.mlb
-    }
-    public func getValueType() -> UpcomingEventValueType {
-        return UpcomingEventValueType.mlb_team
-    }
-}
-
 public final class MLBEvent : GenericUpcomingEvent {
-    public let teamAway:ClientMLBTeam, teamHome:ClientMLBTeam
+    public let team_away:ClientMLBTeam, team_home:ClientMLBTeam
     
-    public init(exactStartMilliseconds: Int64, exactEndMilliseconds: Int64, title: String, description: String?, location: String?, imageURL: String?, sources: EventSources, hyperlinks: Hyperlinks?, countries: [Country]?, subdivisions: [any SovereignStateSubdivision]?, teamAway: ClientMLBTeam, teamHome: ClientMLBTeam) {
-        self.teamAway = teamAway
-        self.teamHome = teamHome
-        super.init(type: UpcomingEventType.sport_mlb, event_date: nil, exact_start: exactStartMilliseconds, exact_end: exactEndMilliseconds, custom_type_singular_name: nil, title: title, description: description, location: location, image_url: imageURL, youtube_video_ids: nil, sources: sources, hyperlinks: hyperlinks, countries: countries, subdivisions: subdivisions)
+    public init(exact_start: Int64, exact_end: Int64, title: String, description: String?, location: String?, image_url: String?, sources: EventSources, hyperlinks: Hyperlinks?, countries: [Country]?, subdivisions: [any SovereignStateSubdivision]?, team_away: ClientMLBTeam, team_home: ClientMLBTeam) {
+        self.team_away = team_away
+        self.team_home = team_home
+        super.init(type: UpcomingEventType.sport_mlb, event_date: nil, exact_start: exact_start, exact_end: exact_end, custom_type_singular_name: nil, title: title, description: description, location: location, image_url: image_url, youtube_video_ids: nil, sources: sources, hyperlinks: hyperlinks, countries: countries, subdivisions: subdivisions)
     }
     
     public required init(from decoder: Decoder) throws {
         let container:KeyedDecodingContainer = try decoder.container(keyedBy: MLBEventCodingKeys.self)
-        teamAway = try container.decode(ClientMLBTeam.self, forKey: .teamAway)
-        teamHome = try container.decode(ClientMLBTeam.self, forKey: .teamHome)
+        team_away = try container.decode(ClientMLBTeam.self, forKey: .team_away)
+        team_home = try container.decode(ClientMLBTeam.self, forKey: .team_home)
         try super.init(from: decoder)
     }
     
     public override func getValue(_ key: any UpcomingEventValueKeys) -> Any? {
         guard let key:MLBEventCodingKeys = key as? MLBEventCodingKeys else { return nil }
         switch key {
-        case .teamAway: return teamAway
-        case .teamHome: return teamHome
+        case .team_away: return team_away
+        case .team_home: return team_home
         }
+    }
+}
+
+public enum MLBEventCodingKeys : String, UpcomingEventValueKeys {
+    case team_away
+    case team_home
+    
+    public func getCategory() -> UpcomingEventValueCategory {
+        return UpcomingEventValueCategory.mlb
+    }
+    public func getValueType() -> UpcomingEventValueType {
+        return UpcomingEventValueType.mlb_team
     }
 }
