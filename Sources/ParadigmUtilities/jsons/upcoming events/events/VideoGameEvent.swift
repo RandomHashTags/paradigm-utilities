6af315c6f24246cafdc80c7cc75be9f3a6b300fa
RandomHashTags <ImRandomHashTags@gmail.com>
2023-01-23 16:34:56 -0600

    updated variable name syntax for `VideoGameEvent`

--- Sources/ParadigmUtilities/jsons/upcoming events/events/VideoGameEvent.swift
+++ Sources/ParadigmUtilities/jsons/upcoming events/events/VideoGameEvent.swift
@@ -8,7 +8,32 @@
 import Foundation
 import SwiftSovereignStates
 
-public enum VideoGameEventCodingKeys : String, UpcomingEventValueKeys {
+public final class VideoGameEvent : GenericUpcomingEvent {
+    public let platforms:[String], genres:[String]
+    
+    public init(event_date: EventDate, title: String, description: String?, location: String?, image_url: String?, youtube_video_ids: [String]?, sources: EventSources, hyperlinks: Hyperlinks?, countries: [Country]?, subdivisions: [any SovereignStateSubdivision]?, platforms: [String], genres: [String]) {
+        self.platforms = platforms
+        self.genres = genres
+        super.init(type: UpcomingEventType.video_game, event_date: event_date, title: title, description: description, location: location, image_url: image_url, sources: sources, hyperlinks: hyperlinks, countries: countries, subdivisions: subdivisions)
+    }
+    
+    public required init(from decoder: Decoder) throws {
+        let container:KeyedDecodingContainer = try decoder.container(keyedBy: VideoGameEventValueKeys.self)
+        platforms = try container.decode([String].self, forKey: .platforms)
+        genres = try container.decode([String].self, forKey: .genres)
+        try super.init(from: decoder)
+    }
+    
+    public override func getValue(_ key: any UpcomingEventValueKeys) -> Any? {
+        guard let key:VideoGameEventValueKeys = key as? VideoGameEventValueKeys else { return nil }
+        switch key {
+        case .platforms: return platforms
+        case .genres: return genres
+        }
+    }
+}
+
+public enum VideoGameEventValueKeys : String, UpcomingEventValueKeys {
     case platforms
     case genres
     
@@ -18,31 +43,6 @@ public enum VideoGameEventCodingKeys : String, UpcomingEventValueKeys {
             return UpcomingEventValueCategory.video_game_details
         case .genres:
             return UpcomingEventValueCategory.video_game_genres
-        }
-    }
-}
-
-public final class VideoGameEvent : GenericUpcomingEvent {
-    public let platforms:[String], genres:[String]
-    
-    public init(eventDate: EventDate, title: String, description: String?, location: String?, imageURL: String?, youtubeVideoIDs: [String]?, sources: EventSources, hyperlinks: Hyperlinks?, countries: [Country]?, subdivisions: [any SovereignStateSubdivision]?, platforms: [String], genres: [String]) {
-        self.platforms = platforms
-        self.genres = genres
-        super.init(type: UpcomingEventType.video_game, event_date: eventDate, title: title, description: description, location: location, image_url: imageURL, sources: sources, hyperlinks: hyperlinks, countries: countries, subdivisions: subdivisions)
-    }
-    
-    public required init(from decoder: Decoder) throws {
-        let container:KeyedDecodingContainer = try decoder.container(keyedBy: VideoGameEventCodingKeys.self)
-        platforms = try container.decode([String].self, forKey: .platforms)
-        genres = try container.decode([String].self, forKey: .genres)
-        try super.init(from: decoder)
-    }
-    
-    public override func getValue(_ key: any UpcomingEventValueKeys) -> Any? {
-        guard let key:VideoGameEventCodingKeys = key as? VideoGameEventCodingKeys else { return nil }
-        switch key {
-        case .platforms: return platforms
-        case .genres: return genres
         }
     }
 }
