e40279c4c26530f919ce8fe572821eae11fcf3f8
RandomHashTags <ImRandomHashTags@gmail.com>
2023-04-17 16:06:01 -0500

    made `world_rank_max` in `SovereignStateRankedValue` settable due to limiting server functionality

--- Sources/ParadigmUtilities/jsons/countries/information/values/SovereignStateRankedValue.swift
+++ Sources/ParadigmUtilities/jsons/countries/information/values/SovereignStateRankedValue.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public protocol SovereignStateRankedValue : SovereignStateIdentifiableValue {
     var world_rank:Int { get }
-    var world_rank_max:Int { get }
+    var world_rank_max:Int { get set }
     var value:RoundedFloat { get }
     var suffix:String? { get }
     var is_estimate:Bool { get }
