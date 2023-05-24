41acfc38bb47b6173b1dcb651b8c2c58e7ed486a
RandomHashTags <ImRandomHashTags@gmail.com>
2023-05-24 17:45:52 -0500

    replaced `String` with `AnyHashable` parameter in `ParadigmSharedInstance`

--- Sources/ParadigmUtilities/util/ParadigmSharedInstance.swift
+++ Sources/ParadigmUtilities/util/ParadigmSharedInstance.swift
@@ -15,13 +15,13 @@ public extension ParadigmSharedInstance {
     static var shared : Self {
         return get(identifier: String(describing: Self.self))
     }
-    static func get<T : ParadigmSharedInstance>(identifier: String) -> T {
+    static func get<T : ParadigmSharedInstance>(identifier: AnyHashable) -> T {
         return ParadigmCache.get_or_load_cache(api_version: APIVersion.latest, type: ParadigmCacheType.shared_instances).get_or_insert(identifier) {
             return T.init()
         }
     }
     
-    static func remove(identifier: String) {
+    static func remove(identifier: AnyHashable) {
         ParadigmCache.remove(api_version: APIVersion.latest, type: ParadigmCacheType.shared_instances, identifier: identifier)
     }
     func remove() {
